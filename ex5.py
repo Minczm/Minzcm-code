27fc19d28c990c46bf1fca5fe10639492889dbbe
Minzcm <2598719590@qq.com>
2022-03-12 15:17:57 +0800

    1.0

---
+++ ex5.py
@@ -0,0 +1,27 @@
+# 名字
+my_name = "Minzcm"
+# 年龄
+my_age = 20
+# 身高
+my_height = 160 #cm
+# 体重
+my_wehight = 45 #kg
+# 眼睛
+my_eyes = "蓝色"
+# 牙齿
+my_teeth = "白色"
+# 头发
+my_hair = "black"
+#介绍我
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_wehight} pounds heavy.")
+print("Acyually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#计算总分
+total = my_age + my_height + my_wehight
+print(f"If I add {my_age}, {my_height}, and {my_wehight} I get {total}.")
+
+
