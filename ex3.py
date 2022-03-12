05293d37bc0ba857321dde34b67aa77969f6e139
Minzcm <2598719590@qq.com>
2022-03-12 12:46:50 +0800

    1.0

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:")
+#显示母鸡数量，公鸡数量。
+print("Hens", 25+30/6)
+print("Roosters", 100-25*3%4)
+#显示我准备计算鸡蛋数了。
+print("Now I will count the eggs:")
+#计算鸡蛋数
+print( 3.0 + 2.0 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+#计算不等式的结果
+print("Is it true that 3 + 2 < 5 - 7 ?")
+#显示结果
+print(3 + 2 < 5 - 7 )
+#计算数字相加减
+print("What is 3 + 2?", 3 + 2 )
+print("What is 5 - 7?", 5 - 7 )
+#显示问题
+print("Oh,that's why it's False.")
+#显示让我们算更多
+print("How about some more.")
+#显示三个计算的结果
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5.00 <= -2.00)
